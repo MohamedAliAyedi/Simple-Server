3e02a4fd162b99edaee5f6a217221f7f257ea0bf
Mohamed Ali Ayedi <46815881+MohamedAliAyedi@users.noreply.github.com>
2019-01-23 22:18:12 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-print("\t\t***Simple Server By Ayedi***")
 import socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 host = socket.gethostname()
 port = 8080
 serversocket.bind((host, port))
 serversocket.listen(5)
-
 while True:
     clientsocket,address = serversocket.accept()
     print("Connection From : " % str(address))
-    message = 'welcome in my server' + "\r\n"
-    clientsocket.send(message)
+    clientsocket.send("Fine")
     clientsocket.close()
